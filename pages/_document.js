d7961addc716b6d27ecdd75dbba601dba3ea967a
morningstar011201 <mr.morningstar011201@gmail.com>
2026-03-27 23:39:11 +0530

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta name="google-site-verification" content="M2_8RuW5xvPYqx6fExNogpdeJEUjrAl7MefiTG7UgWc" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
