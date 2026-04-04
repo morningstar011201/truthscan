bdd09546686a4cc54e4a47ee008599377c364dc8
morningstar011201 <mr.morningstar011201@gmail.com>
2026-04-04 15:23:44 +0530

    Create robots.txt.js

---
+++ pages/robots.txt.js
@@ -0,0 +1,15 @@
+export default function Robots() {}
+
+export async function getServerSideProps({ res }) {
+  res.setHeader("Content-Type", "text/plain");
+  res.write(`User-agent: *
+Allow: /
+
+Disallow: /admin
+Disallow: /profile
+Disallow: /api/
+
+Sitemap: https://truthscan.fun/sitemap.xml`);
+  res.end();
+  return { props: {} };
+}
