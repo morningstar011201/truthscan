2f0be00bbd194f318f4c5488e055b76b197358a4
morningstar011201 <mr.morningstar011201@gmail.com>
2026-04-05 00:50:49 +0530

    Update sitemap.xml.js

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -4,12 +4,12 @@ export async function getServerSideProps({ res }) {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <loc>https://truthscan.fun</loc>
+    <loc>https://www.truthscan.fun</loc>
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
   </url>
   <url>
-    <loc>https://truthscan.fun/pricing</loc>
+    <loc>https://www.truthscan.fun/pricing</loc>
     <changefreq>weekly</changefreq>
     <priority>0.8</priority>
   </url> 
@@ -24,17 +24,17 @@ export async function getServerSideProps({ res }) {
     <priority>0.8</priority>
   </url> 
   <url>
-    <loc>https://truthscan.fun/blog</loc>
+    <loc>https://www.truthscan.fun/blog</loc>
     <changefreq>weekly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>https://truthscan.fun/blog/why-she-replies-late</loc>
+    <loc>https://www.truthscan.fun/blog/why-she-replies-late</loc>
     <changefreq>monthly</changefreq>
     <priority>0.7</priority>
   </url>
   <url>
-    <loc>https://truthscan.fun/blog/mixed-signals-over-text</loc>
+    <loc>https://www.truthscan.fun/blog/mixed-signals-over-text</loc>
     <changefreq>monthly</changefreq>
     <priority>0.7</priority>
   </url>
